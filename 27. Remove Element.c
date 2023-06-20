980285c0919f13ce224862d4b2de784d5e229b96
Prarthana Prasanna Kumar <73553310+Prar2902@users.noreply.github.com>
2023-06-20 23:02:18 +0530

    Update 27. Remove Element.c
    C solution for the 27th question in that Leetcode interview Prep questions..

--- 27. Remove Element.c
+++ 27. Remove Element.c
@@ -1,1 +1,13 @@
-
+//Writing the function removeElement to carry out the needful..
+int removeElement(int* nums, int numsSize, int val){
+    int i, k = 0;
+    for (int i = 0;i<numsSize;++i)
+    {
+        if(nums[i] != val)
+        {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+    return k;
+}
